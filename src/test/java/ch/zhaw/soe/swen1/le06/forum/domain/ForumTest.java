b95f0941e76b916b138fedf5122ca24bed1d26f2
eicw <eicw@zhaw.ch>
2020-07-01 14:14:10 +0200

    Tests auf JUnit 5 migriert.

--- src/test/java/ch/zhaw/soe/swen1/le06/forum/domain/ForumTest.java
+++ src/test/java/ch/zhaw/soe/swen1/le06/forum/domain/ForumTest.java
@@ -1,28 +1,22 @@
 package ch.zhaw.soe.swen1.le06.forum.domain;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * Combined tests for forum domain logic. Because domain classes are quite simple, 
  * only integration tests have been written. 
-  *
- */
+ *
+*/
 public class ForumTest {
 	private Forum forum;
 	private User user;
 	private Instant reference;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		reference = Instant.ofEpochSecond(1000000); 
 		// use a clock which returns always the same time, so testing is easy
@@ -30,7 +24,6 @@ public class ForumTest {
 		user = new User("User", new byte[]{0});
 		forum.getUsers().add(user);
 	}
-
 	 
 
 }
